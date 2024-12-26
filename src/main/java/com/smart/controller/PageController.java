2fde1e669f6836962b1e415d15d68aa2a2ee7200
paritoshpranjal <paritoshpranjal3@gmail.com>
2024-12-27 00:57:20 +0530

    feat: inital project setup

---
+++ src/main/java/com/smart/controller/PageController.java
@@ -0,0 +1,32 @@
+package com.smart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("home page handler");
+        model.addAttribute("name", "Substring Tech");
+        model.addAttribute("yt_channel", "learn code");
+        return "home";
+    }
+
+    // about route
+    @RequestMapping("/about")
+    public String aboutPage() {
+        System.out.println("About page loading");
+        return "about";
+    }
+
+    // service route
+    @RequestMapping("/services")
+    public String servicesPage() {
+        System.out.println("Services page loading");
+        return "services";
+    }
+}
