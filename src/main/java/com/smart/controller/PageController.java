5cfa2e1dba852cfaa75c1d49ae7f9de7c6e5a849
paritoshpranjal <paritoshpranjal3@gmail.com>
2024-12-28 23:38:13 +0530

    feat: add header and its respective starter page

--- src/main/java/com/smart/controller/PageController.java
+++ src/main/java/com/smart/controller/PageController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
@@ -29,4 +30,23 @@ public class PageController {
         System.out.println("Services page loading");
         return "services";
     }
+
+    // contact page
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+
+    // contact page
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    // contact page
+    @GetMapping("/register")
+    public String register() {
+        return new String("register");
+    }
+
 }
